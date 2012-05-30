3d71d19460c2dc28c27c4623957feb842baf6ce2
Teddy Wing <ted@teddywing.com>
2012-05-30 19:29:04 -0400

    MessagesController: added a mailer call when a message is sent so an email notification can be sent to the receiving parties.

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -6,6 +6,11 @@ class Inboxes::MessagesController < Inboxes::BaseController
     @message.user = current_user
     @message.discussion = @discussion
     @message.save
+    
+    sender_index = @message.discussion.speakers.index {|speaker| speaker.user_id == current_user.id }
+    other_speakers = @message.discussion.speakers.dup
+    other_speakers.delete_at(sender_index)
+    InboxesMailer.received_message_deliver(other_speakers, User.find(@message.discussion.speakers.at(sender_index).user_id), @message)
 
     respond_to do |format|
       format.html { redirect_to @message.discussion }
